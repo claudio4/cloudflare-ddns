0025da31ccfac634329a6b8d11b5f3fbd3b26052
Claudio Yanes <me@claudio4.com>
2018-11-10 19:00:56 +0000

    Code Beautify

--- cmd/cloudflare-ddns/main.go
+++ cmd/cloudflare-ddns/main.go
@@ -1,45 +1,45 @@
 package main
 
 import (
-	"github.com/claudio4/cloudflare-ddns/pkg/cloudflare"
 	"flag"
-	"os"
 	"fmt"
+	"github.com/claudio4/cloudflare-ddns/pkg/cloudflare"
 	"github.com/claudio4/cloudflare-ddns/pkg/ip"
+	"os"
 )
 
 func main() {
-    var email, apikey, zoneID, domain string
-    flag.StringVar(&email, "email", "", "Your Cloudflare email")
-    flag.StringVar(&apikey, "key", "", "Your Cloudflare API Key")
-    flag.StringVar(&zoneID, "zoneid", "", "Domain's zone ID")
-    flag.StringVar(&domain, "domain", "", "Domain to change")
-    flag.Parse()
-    if email == "" || apikey == "" || zoneID == "" || domain == "" {
-        fmt.Println("Missing parameters: All the following parameters are required")
-        flag.PrintDefaults()
-        os.Exit(1)
-    }
+	var email, apikey, zoneID, domain string
+	flag.StringVar(&email, "email", "", "Your Cloudflare email")
+	flag.StringVar(&apikey, "key", "", "Your Cloudflare API Key")
+	flag.StringVar(&zoneID, "zoneid", "", "Domain's zone ID")
+	flag.StringVar(&domain, "domain", "", "Domain to change")
+	flag.Parse()
+	if email == "" || apikey == "" || zoneID == "" || domain == "" {
+		fmt.Println("Missing parameters: All the following parameters are required")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
 
-    IP, err := ip.Get()
-    if err != nil {
-        fmt.Fprintln(os.Stderr, err.Error())
-        os.Exit(10)
-    }
+	IP, err := ip.Get()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(10)
+	}
 
-    id, content, ttl, err := cloudflare.GetRecordDetails(email, apikey, zoneID, domain)
-    if err != nil {
-        fmt.Fprintln(os.Stderr, err.Error())
-        os.Exit(20)
-    }
+	id, content, ttl, err := cloudflare.GetRecordDetails(email, apikey, zoneID, domain)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(20)
+	}
 
-    if content == IP {
-        os.Exit(0)
-    }
+	if content == IP {
+		os.Exit(0)
+	}
 
-    err = cloudflare.SetRecord(email, apikey, zoneID, id, "A", domain, IP, ttl)
-    if err != nil {
-        fmt.Fprintln(os.Stderr, err.Error())
-        os.Exit(30)
-    }
+	err = cloudflare.SetRecord(email, apikey, zoneID, id, "A", domain, IP, ttl)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(30)
+	}
 }
--- pkg/ip/ip.go
+++ pkg/ip/ip.go
@@ -1,24 +1,24 @@
 package ip
 
 import (
-    "strings"
 	"github.com/pkg/errors"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
 //Get Returns your ip
 func Get() (string, error) {
-    res, err := http.Get("https://checkip.amazonaws.com/")
-    if err != nil {
-        return "", errors.Wrap(err, "Error requesting the ip")
-    }
-    defer res.Body.Close()
+	res, err := http.Get("https://checkip.amazonaws.com/")
+	if err != nil {
+		return "", errors.Wrap(err, "Error requesting the ip")
+	}
+	defer res.Body.Close()
 
-    ip, err := ioutil.ReadAll(res.Body)
-    if err != nil {
-        return "", errors.Wrap(err, "Error reading the request")
-    }
+	ip, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", errors.Wrap(err, "Error reading the request")
+	}
 
-    return strings.TrimSpace(string(ip)), nil
+	return strings.TrimSpace(string(ip)), nil
 }
