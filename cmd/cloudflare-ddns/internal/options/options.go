15f3485a46d261b6494078b2a4ed1059bd392db2
Miguel-Dorta <contact@migueldorta.com>
2022-07-10 00:21:38 +0100

    Add character delimiter for env vars

--- cmd/cloudflare-ddns/internal/options/options.go
+++ cmd/cloudflare-ddns/internal/options/options.go
@@ -16,11 +16,11 @@ var (
 type Options struct {
 	APIKey          string        `short:"k" long:"api-key" value-name:"<key>" description:"Cloudflare account Global API Key (requires email to be set) (incompatible with token auth)" env:"CF_API_KEY"`
 	APIToken        string        `short:"t" long:"token" value-name:"<token>" description:"API token" env:"CF_TOKEN"`
-	Domains         []string      `short:"d" long:"domain" value-name:"<domain.tld>" description:"Domains (or subdomains) to be updated" env:"CF_DOMAINS"`
+	Domains         []string      `short:"d" long:"domain" value-name:"<domain.tld>" description:"Domains (or subdomains) to be updated" env:"CF_DOMAINS" env-delim:","`
 	Email           string        `short:"e" long:"email" value-name:"<mail@example.cf>" description:"Cloudflare account email (only necessary for api key auth)" env:"CF_EMAIL"`
 	ForceUpdate     bool          `short:"f" long:"force" description:"Force update even if the IP didn't change" env:"CF_FORCE_UPDATE"`
 	RefreshTime     time.Duration `short:"r" long:"refresh-every" description:"Time between refreshing the IP on the domains (enables daemon mode),must specify unit 's' for seconds, 'm' for minutes, 'h' for hours and 'd' for days, ex: 5m" env:"CF_REFRESH_EVERY"`
-	Resolvers       []string      `long:"resolver" value-name:"<server>:<port>" description:"DNS resolvers to be used" env:"CF_RESOLVER" default-mask:"Cloudflare DNS"`
+	Resolvers       []string      `long:"resolver" value-name:"<server>:<port>" description:"DNS resolvers to be used" env:"CF_RESOLVER" default-mask:"Cloudflare DNS" env-delim:","`
 	LogInJSONFormat bool          `long:"json-log" description:"Format log as JSON" env:"CF_JSON_LOG"`
 	OnlyIPv4        bool          `short:"4" long:"only-ipv4" description:"Only use IPv4 (A records)" env:"CF_ONLY_IPV4"`
 	OnlyIPv6        bool          `short:"6" long:"only-ipv6" description:"Only use IPv6" env:"CF_ONLY_IPV6"`
